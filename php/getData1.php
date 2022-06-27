be69e4dbf011e116c32ea61fd1633d205c894182
crnizmaj12 <office.inint@gmail.com>
2022-06-27 23:30:28 +0200

    build

--- php/getData1.php
+++ php/getData1.php
@@ -2,15 +2,15 @@
 header('Content-Type:application/json');
 
 
-$servername = "sql11.freesqldatabase.com";
-$username = "sql11483084";
-$password = "sql11483084";
-$dbname = "sql11483084";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "admin";
 
 // Create connection
 $conn = mysqli_connect($servername,$username,$password,$dbname) or die("Connection failed: " . mysqli_connect_error());
 
-$sql = "SELECT * FROM TABLE_NAME";
+$sql = "SELECT * FROM TABLE_NAME1";
 $result = mysqli_query($conn,$sql);
 $data = array();
 foreach ($result as $row){
--- php/getData3.php
+++ php/getData3.php
@@ -5,12 +5,12 @@ header('Content-Type:application/json');
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "entain";
+$dbname = "admin";
 
 // Create connection
 $conn = mysqli_connect($servername,$username,$password,$dbname) or die("Connection failed: " . mysqli_connect_error());
 
-$sql = "SELECT * FROM Table3";
+$sql = "SELECT * FROM TABLE_NAME3";
 $result = mysqli_query($conn,$sql);
 $data = array();
 foreach ($result as $row){
